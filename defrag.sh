2f0f7cff8951278312fb8d3a58875adf7ee49f45
ness993 <n.horvat7@gmail.com>
2018-07-05 10:56:06 +0200

    Update of scrypt todefrag all drives in /dev/*

--- defrag.sh
+++ defrag.sh
@@ -7,12 +7,15 @@ echo "*********Test if you have a e4defrag**********"
 echo "**********************************************"
 echo "*******Upddate and upgrade e4defrag***********"
 echo "**********************************************"
+echo ""
+echo ""
+read -p "Press Any key to Continiue with Update and upgrade of e4defrag"
 sudo apt-get update && sudo apt-get install e4defrag
 clear
 echo "*************************************************"
-echo "****Give me disk or directory to defrag**********"
-echo "*if you dont knoe wher is disk run sudo fdisk -l*"
+echo "******Give me disk or directory to defrag********"
+echo "%%%%%%This weel defrag all in /dev/*%%%%%%%%%%%%%"
 echo "*************************************************"
-read $lokacija
-sudo e4defrag -v $lokacija
+read -p "Press ENTER to Continiue with e4defrag"
+sudo e4defrag -v /dev/*
 exit
